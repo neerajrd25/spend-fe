61517b1c1d7e409be10ddffc554d1168e4c669e4
neerajrd25 <getnvkarma@gmail.com>
2024-12-26 17:26:43 +0530

    package updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import clsx from 'clsx'
+
 
 export default function Home() {
   return (
@@ -22,7 +24,6 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -45,7 +46,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docs s
           </a>
         </div>
       </main>
